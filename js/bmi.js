738f738f7784ebd0701296c03e27c58de00ac9c5
Daniel <base30904@gmail.com>
2024-01-24 23:49:20 +0800

    完成BMI計算器

--- js/bmi.js
+++ js/bmi.js
@@ -16,14 +16,36 @@ function calBmi() {
         alert("輸入不能為空");
         return;
     }
-    let bmi = getBmi(heigth, heigth);
+    let bmi = getBmi(heigth, weigth);
+    bmiE1.innerText = "BMI:" + bmi;
     console.log(bmi);
+
+    if (bmi > 27) {
+        commentEl.innerText = '肥胖';
+    } else if (bmi <= 27 && bmi > 24) {
+        commentEl.innerText = '過重';
+    } else if (bmi <= 24 && bmi > 18.5) {
+        commentEl.innerText = '健康';
+    } else {
+        commentEl.innerText = '過輕';
+    }
+
+}
+
+function clearForm() {
+    userNameEl.value = "";
+    heigthEl.value = "";
+    weigthEl.value = "";
+    bmiE1.innerText = "";
+    commentEl.innerText = "";
 }
 
 const userNameEl = document.querySelector("#username");
 const heigthEl = document.querySelector("#heigth");
 const weigthEl = document.querySelector("#weigth");
+const bmiE1 = document.querySelector("#bmi");
+const commentEl = document.querySelector("#comment");
 
-console.log(userNameEl, heigthEl, weigthEl);
-console.log(getBmi(heigthEl.value, weigthEl.value));
+//console.log(userNameEl, heigthEl, weigthEl);
+//console.log(getBmi(heigthEl.value, weigthEl.value));
 
